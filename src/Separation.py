44822864716f63f526d04c27353982127ec5aa96
shreybohra <42218014+shreybohra@users.noreply.github.com>
2022-01-22 12:22:54 +0000

    averaging pseudocode

--- src/Separation.py
+++ src/Separation.py
@@ -1,8 +1,18 @@
+from time import sleep
+readings = 100
+
 def altidude(pressure):
      multiplier = 44307.69396     
      alt = multiplier * (1 - (pressure/1013.25)**0.190284)
-
      return alt
      
 print(altidude(1000))
+
+accumulator = 0
+for i in range(readings):
+    get(pressure)
+    accumulator += pressure
+    sleep(1)
+
+avg_pressure = accumulator/readings
 
