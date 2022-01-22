4040e1ff6f030e8e1b9b6c0434d2a3d2c970125a
shreybohra <42218014+shreybohra@users.noreply.github.com>
2022-01-22 12:17:57 +0000

    create altitude function

--- src/Separation.py
+++ src/Separation.py
@@ -1,1 +1,8 @@
-print('Hello Shreya')
+def altidude(pressure):
+     multiplier = 44307.69396     
+     alt = multiplier * (1 - (pressure/1013.25)**0.190284)
+
+     return alt
+     
+print(altidude(1000))
+
