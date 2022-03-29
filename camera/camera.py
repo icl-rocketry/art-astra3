8a7e8f3b2e8c38424f3f1289731a2412209a0270
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-03-29 13:49:16 +0100

    Added gpio based stopping

--- camera/camera.py
+++ camera/camera.py
@@ -1,6 +1,12 @@
 import picamera
+import RPi.GPIO as GPIO
+
+SHORT_PIN = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SHORT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 with picamera.PiCamera(resolution="1296x972", framerate=40) as camera:
-    camera.start_recording('video.h264')
-    camera.wait_recording(10)
+    camera.start_recording('whooosh.h264')
+    while not GPIO.input(SHORT_PIN):
+        camera.wait_recording(10)
     camera.stop_recording()
