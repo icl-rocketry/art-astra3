005cd386f69d8d73a9d5bbdf4df6c0436d0a210d
Akshat-Tripathi <akshat.tripathi6568@gmail.com>
2022-04-02 22:00:08 +0100

    Updated trigger to take in data

--- separation/sw/src/trigger.py
+++ separation/sw/src/trigger.py
@@ -1,23 +1,19 @@
-# In this file we have a Trigger class, which takes in a few FilteredPressureSensors and a separation altitude threshold
+# In this file we have a Trigger class, which takes in a the number of sensors and a separation altitude threshold
 # It has a canSeparate method which returns whether or not we can trigger separation.
 # We only trigger separation if we're below separation threshold, and we believe that the rocket is going down.
-
-from .filter import FilteredPressureSensor  # type: ignore
 
 UP = 0
 DOWN = 1
 
 
 class ApogeeTrigger:
-    def __init__(self, sensors: list[FilteredPressureSensor], threshold: int, pressure_threshold: float):
-        self._sensors = sensors
-        self._old_readings = [0] * len(sensors)
+    def __init__(self, n_sensors: int, threshold: int, pressure_threshold: float):
+        self._old_readings = [0] * n_sensors
         self._threshold = threshold
         self._pressure_threshold = pressure_threshold
         self._down_combo = 0
 
-    def canSeparate(self) -> bool:
-        readings = [s.read() for s in self._sensors]
+    def canSeparate(self, readings: list[float]) -> bool:
         if readings == self._old_readings:
             return False
 
