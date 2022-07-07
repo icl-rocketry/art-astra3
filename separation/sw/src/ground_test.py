4def5f2b439c05dbd534a1248d0850249eab612e
shreybohra <42218014+shreybohra@users.noreply.github.com>
2022-07-07 01:30:50 +0100

    setup servo pwm output

--- separation/sw/src/ground_test.py
+++ separation/sw/src/ground_test.py
@@ -3,6 +3,7 @@ import board
 import neopixel
 from analogio import AnalogIn
 from digitalio import DigitalInOut, Direction
+import pwmio
 from speaker import Speaker
 
 # define pins
@@ -11,7 +12,7 @@ buzzer = Speaker(Aout = board.MISO)
 led = neopixel.NeoPixel(board.NEOPIXEL, 1)
 serpow = DigitalInOut(board.A3)
 serpow.direction = Direction.OUTPUT
-servo = board.A2
+servo = pwmio.PWMOut(board.A2, duty_cycle=0, frequency=330, variable_frequency=False)
 
 def get_voltage():
     reading = (vin.value * 3.3) / 65536
