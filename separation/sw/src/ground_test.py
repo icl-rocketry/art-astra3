e5351ec21adc19d4fc0c309663d4928ad5b6ff31
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2023-02-25 11:44:42 +0000

    removed stuff

--- separation/sw/src/ground_test.py
+++ separation/sw/src/ground_test.py
@@ -1,45 +1,20 @@
 import time
 import board
-import neopixel
-from analogio import AnalogIn
-from digitalio import DigitalInOut, Direction
 import pwmio
-from speaker import Speaker
 
 # define pins
-vin = AnalogIn(board.A1)
-buzzer = Speaker(Aout = board.MISO)
-led = neopixel.NeoPixel(board.NEOPIXEL, 1)
-serpow = DigitalInOut(board.A3)
-serpow.direction = Direction.OUTPUT
-servo = pwmio.PWMOut(board.A2, duty_cycle=0, frequency=330, variable_frequency=False)
-
-def get_voltage():
-    reading = (vin.value * 3.3) / 65536
-    R1 = 220000
-    R2 = 120000
-    return (reading/R2) * (R1+R2)
+servo = pwmio.PWMOut(board.GP3, duty_cycle=0, frequency=330, variable_frequency=False)
 
 def move(position):
-    led.fill((0, 255, 0))
-    buzzer.beep(660)
-    serpow.value = True
+    print("Started movement")
     servo.duty_cycle = (65535 * position) // 100
     time.sleep(0.5)
-    serpow.value = False
-    led.fill((0, 0, 255))
-    buzzer.shutup()
+    print("Ended movement")
 
-
-
-
-while True:    
+while True:
     try:
         position = int(input("Enter position: "))
         move(position)
     except ValueError:
         print("Invalid input - range is 0 to 100")
-        led.fill((255, 0, 0))
-        buzzer.beep(440)
-    print (f"Voltage: {get_voltage()}")
     time.sleep(1)
